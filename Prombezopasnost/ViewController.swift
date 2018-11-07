6e871c8297424ebd05802759892130e41e13f5c4
Altyn Dmitry <altynd@macbook-pro-altyn.local>
2018-11-07 09:30:32 +0300

    Initial Commit

---
+++ Prombezopasnost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prombezopasnost
+//
+//  Created by Altyn Dmitry on 07/11/2018.
+//  Copyright © 2018 Altyn Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

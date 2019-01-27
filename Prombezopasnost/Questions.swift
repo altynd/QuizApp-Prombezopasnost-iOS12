659cdef4543bc9be6f4eddb59749e863641c91dc
Сэр Дъмитрий <altynd@mail.ru>
2019-01-27 21:01:57 +0300

    +

---
+++ Prombezopasnost/Questions.swift
@@ -0,0 +1,17 @@
+//
+//  Questions.swift
+//  Prombezopasnost
+//
+//  Created by Altyn Dmitry on 27/01/2019.
+//  Copyright © 2019 Altyn Dmitry. All rights reserved.
+//
+
+import Foundation
+
+// задаем вопросы
+let voprosiArray = ["Favorite color?", "Favorite pet?", "Whoes your Daddy?"]
+
+// ответы
+let otvetiArray = [["1red", "2green", "3blue", "4orange","5yellow",],["1dog", "2cat", "3bird", "4chikken",""],["1", "2", "3","",""]]
+
+let pravelniyOtvetIndexArray = [2,2,3]
--- Prombezopasnost/ViewController.swift
+++ Prombezopasnost/ViewController.swift
@@ -13,8 +13,77 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBOutlet weak var kursNomer: UIButton!
+    @IBOutlet weak var biletNomer: UIButton!
+    @IBOutlet weak var voprosNomer: UIButton!
+    @IBOutlet weak var voprosText: UILabel!
+    @IBOutlet var otvetText: [UIButton]!
 
+   
+    @IBAction func viborKurs(_ sender: UIButton) {
+    }
+    @IBAction func viborBilet(_ sender: UIButton) {
+    }
+    @IBAction func viborVopros(_ sender: UIButton) {
+    }
+    @IBAction func otvetKnopki(_ sender: UIButton) {
+        if sender.tag == praviniyOtvet{
+            print("Правильный ответ")
+        } else{
+            print("Не правильный ответ")
+        }
+        
+        if nomerVoprosa != vsegoVoprosov - 1{
+            newQuestion()
+        }
+    }
+    @IBAction func nazad(_ sender: UIButton) {
+    }
+    @IBAction func menu(_ sender: UIButton) {
+    }
+    @IBAction func vpered(_ sender: UIButton) {
+    }
+    
+    //
+    override func viewDidAppear(_ animated: Bool) {
+        newQuestion()
+    }
+    
+    // задаем значения для первого вопроса
+    var nomerVoprosa = 0
+    let vsegoVoprosov = voprosiArray.count
+    var praviniyOtvet = pravelniyOtvetIndexArray[0]
+    
+    // функция показывает верные ответы
+    func newQuestion(){
+        
+        // задаем текст вопроса в лейбл
+        voprosText.text = voprosiArray[nomerVoprosa]
+        
+        
+        var button:UIButton = UIButton()
+        let vsegoOtvetov = otvetiArray[nomerVoprosa].count
+        
+        for i in 1...(vsegoOtvetov)
+        {
+            // задаем текст ответов в кнопки
+            button = view.viewWithTag(i) as! UIButton
+            button.setTitle(otvetiArray[nomerVoprosa][i-1], for: .normal)
+            
+            //указваем правильный ответ для каждого вопроса
+            
+//
+//            if (i == rightAnswerPlacement){
+//                button.setTitle(answers[currentQuestion][i-1], for: .normal)
+//            } else{
+//                button.setTitle("", for: .normal)
+//            }
+//            currentQuestion += 1
+        }
+    }
+    
 }
 

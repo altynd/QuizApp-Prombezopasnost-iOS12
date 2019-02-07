3101a56e77b8af64e550cb4257e93db8931cd6a3
Сэр Дъмитрий <altynd@mail.ru>
2019-02-07 13:45:14 +0300

    результат экзамена

--- Prombezopasnost/SecondViewController.swift
+++ Prombezopasnost/SecondViewController.swift
@@ -17,11 +17,21 @@ class SecondViewController: UIViewController/*ViewController*/ {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                rezultati.text = "Правильных ответов: \(points ?? 0) из \(vsegoVoprosov ?? 0)"
+                rezultati.text = "Правильных ответов: \(points ?? 0) из \(vsegoVoprosov ?? 0)\n\(result())"
     }
     
     @IBOutlet weak var rezultati: UILabel!
     
-    
+    func result()->String{
+        points = points ?? 0
+        vsegoVoprosov = vsegoVoprosov ?? 0
+        if points! > 0 && points == vsegoVoprosov{
+           return("\nОтличный результат.\nЭкзамен пройден.")
+        } else if points! >= vsegoVoprosov! - 1{
+            return("\nХороший результат.\nЭкзамен пройден.")
+        }else{
+            return("\n\nЭкзамен не пройден.")
+        }
+    }
     
 }
